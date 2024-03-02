022d3c320d46fdc38028ca9a3d4d7333ddb0995c
MNAth_ <neodarksoulink@gmail.com>
2024-03-02 08:35:17 +0300

    rerfactor: handle empty page & theme management

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,18 +1,47 @@
-import { SignIn } from "@clerk/nextjs";
+"use client";
+
+import { useContext, useEffect, useRef } from "react";
 import { unstable_noStore as noStore } from "next/cache";
+import { useRouter } from "next/navigation";
+import { SignIn } from "@clerk/nextjs";
+
+// PROVIDERS
+import { UserContext } from "~/components/provider/user-provider";
 
 const SignInPage = () => {
   noStore();
 
+  // HOOKS
+  const router = useRouter();
+  const currentUser = useContext(UserContext);
+
+  // REFS
+  const mainRef = useRef<HTMLElement | null>(null);
+
+  useEffect(() => {
+    // Should refresh the page if the sign-in is not available...
+    setTimeout(() => {
+      if (!currentUser?.isSignedIn && !mainRef.current?.firstChild?.firstChild)
+        location.reload();
+    }, 2000);
+  }, [currentUser?.isSignedIn, router]);
+
   return (
-    <main className="fixed left-0 top-0 flex h-dvh w-screen items-center justify-center bg-white">
+    <main
+      className="fixed left-0 top-0 flex h-dvh w-screen items-center justify-center"
+      ref={mainRef}
+    >
       <SignIn
         appearance={{
           elements: {
-            card: "shadow-none",
-            footerAction__signIn: "text-gray-700 font-medium",
-            footerAction__signUp: "text-gray-700 font-medium",
-            headerTitle: "before:content-['Join_the_best_community._']",
+            card: "shadow-none dark:bg-dark [&>.cl-internal-1wmikye]:hidden",
+            headerTitle:
+              "before:content-['Join_the_best_community._'] text-gray-700 dark:text-gray-50",
+            headerSubtitle: "text-gray-700 dark:text-gray-50",
+            socialButtonsBlockButton: "text-gray-700 dark:text-gray-50",
+            footerActionText: "font-medium text-gray-700 dark:text-gray-50",
+            footerActionLink:
+              "font-bold text-gray-700 hover:text-gray-700 dark:text-gray-50 ring-0 border-none outline-0 !shadow-none",
           },
           layout: {},
           variables: { colorPrimary: "#6366F1" },
